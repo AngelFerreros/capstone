d78b007309b25af57f5ea3eb05cd0cc63247d8b5
Angel Ferreros <AngelFerreros@TJs-MacBook-Pro.local>
2020-03-05 17:28:02 +0800

    Can render relevant data properly

--- controllers/app.js
+++ controllers/app.js
@@ -9,8 +9,10 @@ module.exports = (db) => {
 
   let indexControllerCallback = (request, response) => {
       db.app.getAll((error, allActivities) => {
-        console.log('ALL ACTIVITIES: ', allActivities)
-        response.render('app/index', { allActivities });
+        data = {
+          activities: allActivities
+        }
+        response.render('app/index', data);
       });
   };
 
--- views/app/index.jsx
+++ views/app/index.jsx
@@ -1,13 +1,18 @@
 var React = require("react");
 
 class Index extends React.Component {
+
   render() {
-    // console.log(this.props.types);
+    console.log('Index data: ', this.props.activities);
+    let activityArr = this.props.activities;
+    let activities = activityArr.map((activity,index) => <li key = {index} id = {index}>{activity.title}</li>);
+
     return (
       <html>
         <head />
         <body>
           <h3>Hello</h3>
+          <ul>{activities}</ul>
         </body>
       </html>
     );
