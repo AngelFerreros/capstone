033742e42f0adb2c43b07cb74aa56416413aed00
Angel Ferreros <AngelFerreros@TJs-MacBook-Pro.local>
2020-03-06 15:03:52 +0800

    Created the individual card and can render individual info - need to fix issues with displaying time. Added styles

---
+++ views/app/Nav.jsx
@@ -0,0 +1,31 @@
+const React = require("react");
+
+class Nav extends React.Component {
+  render() {
+
+  return (
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <a id="logo" href= "/">
+              <div className="navbar-brand"><img src="#"/></div>
+            </a>
+
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav mr-auto">
+              <li className="nav-item active">
+                <a className="nav-link" href="#">Discover People <span className="sr-only">(current)</span></a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#"> Organise an Activity</a>
+              </li>
+            </ul>
+                <button type="button" className="btn btn-outline-success" href="#"> Log out</button>
+          </div>
+        </nav>
+    );
+  }
+}
+module.exports = Nav;
--- views/app/index.jsx
+++ views/app/index.jsx
@@ -1,18 +1,64 @@
-var React = require("react");
+const React = require("react");
+const Footer = require("./Footer");
+const Nav = require("./Nav");
+const moment = require('moment');
+moment().format();
 
 class Index extends React.Component {
 
   render() {
     console.log('Index data: ', this.props.activities);
     let activityArr = this.props.activities;
-    let activities = activityArr.map((activity,index) => <li key = {index} id = {index}>{activity.title}</li>);
 
+// create cards for individual activity
+    let card;
+    if (activityArr){
+      card = activityArr.map( (activity,index) => {
+        const title = activity.title
+        const venue = activity.address
+        const date = moment(activity.activity_date.toISOString().split("T")[0]).format("MMM DD YYYY")
+        const start = moment(activity.start_at).format('LT')
+        const end = moment(activity.end_at).format('LT')
+
+
+       // let test = moment.("2020-03-31T16:00:00.000Z").format("MMM DD YYYY")
+        // let formattedDate = moment({month: date.month, date: date.date,year: date.year}).format("MMM DD YYYY");
+        // let formattedStart = moment({start}).format('LT');
+        // let formattedEnd =  moment({end}).format('LT');
+        console.log(title, venue, date, start, end)
+        return (
+        <div className="card">
+          <div className="card-body">
+              <h5 className="card-title">{title}</h5>
+                <p className="card-text">Venue: <span> {venue} </span> </p>
+                <p className="card-text">Date: <span> {date} </span> </p>
+                <p className="card-text">Time: <span> {start} - {end}</span> </p>
+
+                  <a href="#" className="btn view">View More</a>
+          </div>
+        </div>
+        );
+      });
+    }
     return (
       <html>
-        <head />
+        <head>
+          <meta charSet="utf-8"/>
+          <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
+          <title>Game,Set, Match</title>
+          <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossOrigin="anonymous"/>
+          <link rel="stylesheet" href="/styles.css" type = "text/css" />
+        </head>
         <body>
-          <h3>Hello</h3>
-          <ul>{activities}</ul>
+        <Nav/>
+          <div className = "container">
+            {card}
+          </div>
+
+          <Footer/>
+            <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossOrigin="anonymous"></script>
+            <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossOrigin="anonymous"></script>
+            <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossOrigin="anonymous"></script>
         </body>
       </html>
     );
